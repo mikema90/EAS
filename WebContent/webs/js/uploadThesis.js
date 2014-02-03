19eca7421f50393ac1380f840dba7c0670cf7016
Gao Guangyu <330737668@qq.com>
2014-02-03 23:35:36 +0800

    add file uploading function

--- WebContent/webs/js/uploadThesis.js
+++ WebContent/webs/js/uploadThesis.js
@@ -16,8 +16,8 @@ function hideAuthorEditor(){
 function insertAuthorInfo(){
 	var newAuthorInfo=$("#authorInfoTemplate").clone(true);
 	newAuthorInfo.find(".authorOrder").text($("#authorEditor").find(".authorOrder").text());
-	newAuthorInfo.find(".authorName").text($("#authorNameInputBox").val());
-	newAuthorInfo.find(".authorId").text($("#authorIdInputBox").val());
+	newAuthorInfo.find(".authorName").find("div").text($("#authorNameInputBox").val());
+	newAuthorInfo.find(".authorId").find("div").text($("#authorIdInputBox").val());
 	
 	newAuthorInfo.insertBefore($("#authorEditor"));
 	//$("#authorListTable").find("tbody").append(newAuthorInfo);
@@ -59,6 +59,83 @@ function resetRowOrder(){
     });
 }
 
-function delAuthorInfo(){
-	
+function moveUp(targetElement){
+	var targetParent=$(targetElement).closest("tr");
+	var targetIndex=$("#authorListTable tr").index(targetParent);
+	if(targetIndex!=1){
+		targetParent.insertBefore($("#authorListTable tr:eq("+(targetIndex-1)+")"));
+	}
+	resetRowOrder();
+}
+
+function moveDown(targetElement){
+	var targetParent=$(targetElement).closest("tr");
+	var targetIndex=$("#authorListTable tr").index(targetParent);
+	var trNum=$("#authorListTable tr").size();
+	if(targetIndex!=(trNum-3)){
+		targetParent.insertAfter($("#authorListTable tr:eq("+(targetIndex+1)+")"));
+	}
+	resetRowOrder();
+}
+
+function delAuthorInfo(targetElement){
+	$(targetElement).closest("tr").remove();
+	resetRowOrder();
+}
+
+function uploadFile(){
+	$("#uploadingStatus").css("color","#F60");
+	$("#uploadingStatus").text("正在上传...");
+	uploadingStatusNum=3;
+
+	try{
+		$(window.frames["hidden_frame"].document).text("");
+	}catch(e){}
+	checkingInterval=setInterval(checkUploadingStatus, 2000);
+	$("#uploadFileForm").submit();
+}
+
+function uploadingFailure(){
+	$("#uploadingStatus").css("color","#F00");
+	$("#uploadingStatus").text("上传失败");
+}
+
+function changeUploadingStatus(){
+	$("#uploadingStatus").css("color","#F60");
+	$("#uploadingStatus").text("等待上传");
+}
+
+function uploadingSuccess(tempName){
+	$("#fileTempName").val(tempName);
+	$("#uploadingStatus").css("color","#0F0");
+	$("#uploadingStatus").text("上传成功！");
+}
+
+function checkUploadingStatus(){
+	try{
+		var iframeContent=$(window.frames["hidden_frame"].document).text();
+		if(iframeContent.toLowerCase().indexOf("success")>0){
+			clearInterval(checkingInterval);
+		}else if(iframeContent!=""){
+			clearInterval(checkingInterval);
+			uploadingFailure();
+		}else{
+			uploadingStatusNum++;
+			if(uploadingStatusNum==7){
+				uploadingStatusNum=1;
+			}
+			var showWord="正在上传";
+			for(var i=0;i<uploadingStatusNum;i++){
+				showWord=showWord+".";
+			}
+			$("#uploadingStatus").text(showWord);
+		}
+	}catch(e){
+		clearInterval(checkingInterval);
+		uploadingFailure()
+	}
+}
+
+function customFunction2(){
+	alert($(window.frames["hidden_frame"].document).text());
 }
