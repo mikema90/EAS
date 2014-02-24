dcb43375de3054d89a128470412a9c27c0b83a4e
mikema90 <mikema90@gmail.com>
2014-02-24 23:33:34 +0800

    add mapping table

--- sql script/create admin.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE  IF NOT EXISTS `eas` /*!40100 DEFAULT CHARACTER SET utf8 */;
-create user 'easadmin' identified by 'abcdefg';
-grant all on eas.* to easadmin@'%' identified by 'abcdefg';
-grant all on eas.* to easadmin@'localhost' identified by 'abcdefg';
---
+++ src/model/mapping.java
@@ -0,0 +1,18 @@
+package model;
+
+public class mapping {
+	private String issues;
+	private String journal_type;
+	public String getIssues() {
+		return issues;
+	}
+	public void setIssues(String issues) {
+		this.issues = issues;
+	}
+	public String getJournal_type() {
+		return journal_type;
+	}
+	public void setJournal_type(String journal_type) {
+		this.journal_type = journal_type;
+	}
+}
