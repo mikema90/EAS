2c19c223d9f2089fe491c9bd17800b83ffbee797
Gao Guangyu <330737668@qq.com>
2014-02-27 22:36:43 +0800

    fix periodical type error bug

--- WebContent/webs/js/uploadThesis.js
+++ WebContent/webs/js/uploadThesis.js
@@ -1,71 +1,53 @@
 // JavaScript Document
-$(document)
-		.ready(
+$(document).ready(
 
-				function() {
-					var yearSelector = $("select[name='timeYear']");
-					for (var i = 2010; i <= 2030; i++) {
-						var newOption = new Option(i + "", i + "");
-						yearSelector.append(newOption);
-					}
-					var myDate = new Date();
-					yearSelector.find("[value='" + myDate.getFullYear() + "']")
-							.attr("selected", "selected");
+function () {
+	var yearSelector = $("select[name='timeYear']");
+	for (var i = 2010; i <= 2030; i++) {
+		var newOption = new Option(i + "", i + "");
+		yearSelector.append(newOption);
+	}
+	var myDate = new Date();
+	yearSelector.find("[value='" + myDate.getFullYear() + "']").attr("selected", "selected");
 
-					var infoId = getParam("id");
-					if (infoId == null) {
-						submitUrl = "../addPaper";
-						return;
-					} else {
-						thesisInfo = JSON.parse($.cookie(infoId));
-						$("#thesisTypeSelector").find(
-								"option:contains('" + thesisInfo.category
-										+ "')").attr("selected", "selected");
-						insertAuthor2nd(thesisInfo.first_author,
-								thesisInfo.first_author_wid);
-						if (thesisInfo.other_authors != "") {
-							var otherAuthorNames = thesisInfo.other_authors
-									.split(",");
-							var otherAuthorIds = thesisInfo.other_authors_wid
-									.split(",");
-							for (var i = 0; i < otherAuthorNames.length; i++) {
-								insertAuthor2nd(otherAuthorNames[i],
-										otherAuthorIds[i]);
-							}
-						}
-						$("#thesisNameInputBox").val(thesisInfo.title);
-						$("#periodicalNameInputBox").val(thesisInfo.journal);
-						var periodicalSn = thesisInfo.issues.split("-");
-						$("#periodicalSelector").val(periodicalSn[0]);
-						changePeriodicalSnSelector();
-						if (periodicalSn[0] == "ISSN") {
-							$(".periodicalSn.sn2").val(periodicalSn[2]);
-						}
-						$(".periodicalSn.sn1").val(periodicalSn[1]);
-						$(
-								".timeSelector.timeOfYear option[value='"
-										+ thesisInfo.post_date.split("-")[0]
-										+ "']").attr("selected", "selected");
-						$(
-								".timeSelector.timeOfMonth option[value='"
-										+ parseInt(thesisInfo.post_date
-												.split("-")[1]) + "']").attr(
-								"selected", "selected");
-						$(
-								"#languageSelector option:contains('"
-										+ thesisInfo.language + "')").attr(
-								"selected", "selected");
-						$("#uploadingStatus").css("color", "#F60");
-						$("#thesisId").val(thesisInfo.id);
-						$("#uploadingStatus").text("不上传新文件则保留原文件");
-						submitUrl = "../updatePaper";
-					}
-				});
+	var infoId = getParam("id");
+	if (infoId == null) {
+		submitUrl = "../addPaper";
+		return;
+	} else {
+		thesisInfo = JSON.parse($.cookie(infoId));
+		$("#thesisTypeSelector").find("option:contains('" + thesisInfo.category + "')").attr("selected", "selected");
+		insertAuthor2nd(thesisInfo.first_author, thesisInfo.first_author_wid);
+		if (thesisInfo.other_authors != "") {
+			var otherAuthorNames = thesisInfo.other_authors.split(",");
+			var otherAuthorIds = thesisInfo.other_authors_wid.split(",");
+			for (var i = 0; i < otherAuthorNames.length; i++) {
+				insertAuthor2nd(otherAuthorNames[i], otherAuthorIds[i]);
+			}
+		}
+		$("#thesisNameInputBox").val(thesisInfo.title);
+		$("#periodicalNameInputBox").val(thesisInfo.journal);
+		var periodicalSn = thesisInfo.issues.split("-");
+		$("#periodicalSelector").val(periodicalSn[0]);
+		changePeriodicalSnSelector();
+		if (periodicalSn[0] == "ISSN") {
+			$(".periodicalSn.sn2").val(periodicalSn[2]);
+		}
+		$(".periodicalSn.sn1").val(periodicalSn[1]);
+		$(".timeSelector.timeOfYear option[value='" + thesisInfo.post_date.split("-")[0] + "']").attr("selected", "selected");
+		$(".timeSelector.timeOfMonth option[value='" + parseInt(thesisInfo.post_date.split("-")[1]) + "']").attr("selected", "selected");
+		$("#languageSelector option:contains('" + thesisInfo.language + "')").attr("selected", "selected");
+		$("#uploadingStatus").css("color", "#F60");
+		$("#thesisId").val(thesisInfo.id);
+		$("#uploadingStatus").text("不上传新文件则保留原文件");
+		submitUrl = "../updatePaper";
+	}
+});
 
 function insertAuthor2nd(authorName, authorId) {
 	var newAuthorInfo = $("#authorInfoTemplate").clone(true);
 	newAuthorInfo.find(".authorOrder").text(
-			$("#authorEditor").find(".authorOrder").text());
+	$("#authorEditor").find(".authorOrder").text());
 	newAuthorInfo.find(".authorName").find("input").val(authorName);
 	newAuthorInfo.find(".authorId").find("input").val(authorId);
 	newAuthorInfo.insertBefore($("#authorEditor"));
@@ -99,8 +81,7 @@ function insertAuthorInfo() {
 	} else if ($("#authorIdInputBox").val().length > 8) {
 		alert("工号不能超过8位");
 	} else {
-		insertAuthor2nd($("#authorNameInputBox").val(), $("#authorIdInputBox")
-				.val())
+		insertAuthor2nd($("#authorNameInputBox").val(), $("#authorIdInputBox").val())
 		hideAuthorEditor();
 		$("#authorNameInputBox").val("");
 		$("#authorIdInputBox").val("");
@@ -112,9 +93,9 @@ function editAuthorInfo(targetElement) {
 	targetParent.find(".authorName").find("input").removeAttr("readonly");
 	targetParent.find(".authorId").find("input").removeAttr("readonly");
 	targetParent.find(".authorName").find("div").text(
-			targetParent.find(".authorName").find("input").val());
+	targetParent.find(".authorName").find("input").val());
 	targetParent.find(".authorId").find("div").text(
-			targetParent.find(".authorId").find("input").val());
+	targetParent.find(".authorId").find("input").val());
 	targetParent.addClass("editing");
 	targetParent.removeClass("finishEdit");
 }
@@ -133,14 +114,14 @@ function confirmAndQuitEditing(targetElement) {
 function rollbackEditing(targetElement) {
 	var targetParent = $(targetElement).closest("tr");
 	targetParent.find(".authorName").find("input").val(
-			targetParent.find(".authorName").find("div").text());
+	targetParent.find(".authorName").find("div").text());
 	targetParent.find(".authorId").find("input").val(
-			targetParent.find(".authorId").find("div").text());
+	targetParent.find(".authorId").find("div").text());
 	confirmAndQuitEditing(targetElement);
 }
 
 function resetRowOrder() {
-	$("#authorListTable .authorOrder").each(function(index, element) {
+	$("#authorListTable .authorOrder").each(function (index, element) {
 		$(element).text(index + 1);
 	});
 }
@@ -149,8 +130,7 @@ function moveUp(targetElement) {
 	var targetParent = $(targetElement).closest("tr");
 	var targetIndex = $("#authorListTable tr").index(targetParent);
 	if (targetIndex != 1) {
-		targetParent.insertBefore($("#authorListTable tr:eq("
-				+ (targetIndex - 1) + ")"));
+		targetParent.insertBefore($("#authorListTable tr:eq(" + (targetIndex - 1) + ")"));
 	}
 	resetRowOrder();
 }
@@ -160,8 +140,7 @@ function moveDown(targetElement) {
 	var targetIndex = $("#authorListTable tr").index(targetParent);
 	var trNum = $("#authorListTable tr").size();
 	if (targetIndex != (trNum - 3)) {
-		targetParent.insertAfter($("#authorListTable tr:eq("
-				+ (targetIndex + 1) + ")"));
+		targetParent.insertAfter($("#authorListTable tr:eq(" + (targetIndex + 1) + ")"));
 	}
 	resetRowOrder();
 }
@@ -179,7 +158,7 @@ function changePeriodicalSnSelector() {
 	targetParent.addClass("is" + $("#periodicalSelector").val());
 	$(".periodicalSn.sn1").val("");
 	$(".periodicalSn.sn2").val("");
-	$("#periodicalType").val("");
+	$("#periodicalTypeText").val("");
 }
 
 function onChangeSn1(targetElement) {
@@ -208,11 +187,9 @@ function onChangeSn1(targetElement) {
 }
 
 function onChangeSn2(targetElement) {
-	if ($(targetElement).val().length >= 4
-			&& $("#periodicalSelector").val() != "CN") {
+	if ($(targetElement).val().length >= 4 && $("#periodicalSelector").val() != "CN") {
 		$(targetElement).val($(targetElement).val().substr(0, 4));
-	} else if ($(targetElement).val().length >= 7
-			&& $("#periodicalSelector").val() == "CN") {
+	} else if ($(targetElement).val().length >= 7 && $("#periodicalSelector").val() == "CN") {
 		$(targetElement).val($(targetElement).val().substr(0, 7));
 	}
 }
@@ -225,8 +202,7 @@ function uploadFile() {
 
 	try {
 		$(window.frames["hidden_frame"].document).text("");
-	} catch (e) {
-	}
+	} catch (e) {}
 	checkingInterval = setInterval(checkUploadingStatus, 2000);
 	$("#uploadFileForm").submit();
 }
@@ -248,25 +224,26 @@ function uploadingSuccess(tempName) {
 }
 
 function checkPeriodicalSn(targetElement) {
-	if ($("#periodicalNameInputBox").val() != null
-			&& $(".periodicalSn.sn1").val() != null) {
-		$.ajax({
-			type : 'POST',
-			url : '../mapping',
-			data : {
-				periodicalName : $("#periodicalNameInputBox").val(),
-				periodicalType : $("#periodicalSelector").val(),
-				periodicalSn1 : $(".periodicalSn.sn1").val(),
-				periodicalSn2 : $(".periodicalSn.sn2").val(),
-			},
-			success : function(jsonData) {
-				if (jsonData.Status == "success") {
-					$("#periodicalTypeText").val(jsonData.retMsg);
-				}
-			},
-			dataType : 'json'
-		});
+	if (($("#periodicalSelector").val() == "ISSN" || $("#periodicalSelector").val() == "CN") && $(".periodicalSn.sn2").val()=="") {
+		return;
 	}
+	if ($("#periodicalNameInputBox").val()=="" || $(".periodicalSn.sn1").val()=="") {
+		return;
+	}
+	$.ajax({
+		type: 'POST',
+		url: '../mapping',
+		data: {
+			periodicalName: $("#periodicalNameInputBox").val(),
+			periodicalType: $("#periodicalSelector").val(),
+			periodicalSn1: $(".periodicalSn.sn1").val(),
+			periodicalSn2: $(".periodicalSn.sn2").val(),
+		},
+		success: function (jsonData) {
+			$("#periodicalTypeText").val(jsonData.retMsg);
+		},
+		dataType: 'json'
+	});
 }
 
 function checkUploadingStatus() {
@@ -297,42 +274,33 @@ function checkUploadingStatus() {
 function submitForm() {
 	if ($("#authorListTable tbody").find("tr").length < 4) {
 		alert("作者不能为空");
-	} else if ($("#thesisNameInputBox").val() == ""
-			|| $("#periodicalNameInputBox").val() == "") {
+	} else if ($("#thesisNameInputBox").val() == "" || $("#periodicalNameInputBox").val() == "") {
 		alert("论文和期刊名称不能为空");
-	} else if ($("#periodicalSelector").val() == "ISSN"
-			&& ($(".periodicalSn.sn1").val().length != 4 || $(
-					".periodicalSn.sn2").val().length != 4)) {
+	} else if ($("#periodicalSelector").val() == "ISSN" && ($(".periodicalSn.sn1").val().length != 4 || $(".periodicalSn.sn2").val().length != 4)) {
 		alert("ISSN需要8位数字或字母并且由短横隔开");
-	} else if ($("#periodicalSelector").val() == "ISBN"
-			&& $(".periodicalSn.sn1").val().length != 13) {
+	} else if ($("#periodicalSelector").val() == "ISBN" && $(".periodicalSn.sn1").val().length != 13) {
 		alert("ISSN需要13位数字");
-	} else if ($("#periodicalSelector").val() == "CN"
-			&& ($(".periodicalSn.sn1").val().length != 2 || ($(
-					".periodicalSn.sn2").val().length != 2
-					&& $(".periodicalSn.sn2").val().length != 4 && $(
-					".periodicalSn.sn2").val().length != 7))) {
+	} else if ($("#periodicalSelector").val() == "CN" && ($(".periodicalSn.sn1").val().length != 2 || ($(".periodicalSn.sn2").val().length != 2 && $(".periodicalSn.sn2").val().length != 4 && $(".periodicalSn.sn2").val().length != 7))) {
 		alert("CN需要两位+两位字符，或者两位+四位字符");
-	} else if ($("#uploadingStatus").text() != "上传成功！"
-			&& $("#uploadingStatus").text() != "不上传新文件则保留原文件") {
+	} else if ($("#uploadingStatus").text() != "上传成功！" && $("#uploadingStatus").text() != "不上传新文件则保留原文件") {
 		alert("请上传附件");
 	} else {
 		var submitData = $("#thesisInfoForm").serialize();
 		$.ajax({
-			type : 'POST',
-			url : submitUrl,
-			data : submitData,
-			success : function(jsonData) {
+			type: 'POST',
+			url: submitUrl,
+			data: submitData,
+			success: function (jsonData) {
 				if (jsonData.Status == "success") {
 					window.location = jsonData.redirectUrl;
 				} else {
 					alert("保存失败:" + jsonData.retMsg);
 				}
 			},
-			error : function() {
+			error: function () {
 				alert("保存失败");
 			},
-			dataType : 'json'
+			dataType: 'json'
 		});
 	}
 }
