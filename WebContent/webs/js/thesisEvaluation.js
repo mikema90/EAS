b03e69011e48ca4eb6f1653fed7a1896bc8e4e0c
Gao Guangyu <330737668@qq.com>
2014-03-16 19:23:30 +0800

    thesisEvaluation.html half finished

--- WebContent/webs/js/thesisEvaluation.js
+++ WebContent/webs/js/thesisEvaluation.js
@@ -1,4 +1,30 @@
 // JavaScript Document
+$(document).ready(function () {
+	$("#exchangingDataBckgnd").css("visibility", "visible");
+	$("#exchangingDataInnerWrapper").css("visibility", "visible");
+	$.ajax({
+		type: 'POST',
+		url: "../getExpert",
+		success: function (jsonData) {
+			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
+			if (jsonData.Status == "success") {
+				$.each(jsonData.expert, function (idx, itemExpert) {
+					insertNewRow(itemExpert.id, itemExpert.college_id, itemExpert.name, itemExpert.work_id, itemExpert.subject, itemExpert.language);
+				});
+			} else {
+				alert("加载失败");
+			}
+			$("#exchangingDataBckgnd").css("visibility", "hidden");
+		},
+		error: function () {
+			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
+			alert("加载失败");
+			$("#exchangingDataBckgnd").css("visibility", "hidden");
+		},
+		dataType: 'json'
+	});
+});
+
 function cancelRemarkEditor(){
 	$("#remarkEditorBckgnd").css("visibility","hidden");
 	$("#remarkEditorOuterWrapper").css("visibility","hidden");
