c34c614b8573980f23b599083e468ca1f785ba75
Gao Guangyu <330737668@qq.com>
2014-03-16 22:08:44 +0800

    add thesis evaluation function

--- WebContent/webs/js/thesisEvaluation.js
+++ WebContent/webs/js/thesisEvaluation.js
@@ -4,12 +4,16 @@ $(document).ready(function () {
 	$("#exchangingDataInnerWrapper").css("visibility", "visible");
 	$.ajax({
 		type: 'POST',
-		url: "../getExpert",
+		url: "../getEvaluationPaper",
 		success: function (jsonData) {
 			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
 			if (jsonData.Status == "success") {
-				$.each(jsonData.expert, function (idx, itemExpert) {
-					insertNewRow(itemExpert.id, itemExpert.college_id, itemExpert.name, itemExpert.work_id, itemExpert.subject, itemExpert.language);
+				$.each(jsonData.paper, function (idx, itemPaper) {
+							var tmpAuthorName = paperItem.first_author;
+		if (paperItem.other_authors != "") {
+			tmpAuthorName = tmpAuthorName + "," + paperItem.other_authors;
+		}
+					insertNewRow(itemPaper.id, itemPaper.college_name, tmpAuthorName, itemPaper.title, itemPaper.journal, itemPaper.issues, itemPaper.post_date, itemPaper.language ,itemPaper.journal_type, itemPaper.pdf_url, itemPaper.passed);
 				});
 			} else {
 				alert("加载失败");
@@ -25,8 +29,29 @@ $(document).ready(function () {
 	});
 });
 
-function insertNewRow(school, authorName, thesisTitle, periodicalName, periodicalSn, publishTime, thesisLanguage, isCore, pdfId, evalResult, remark) {
-
+function insertNewRow(thesisId, school, authorName, thesisTitle, periodicalName, periodicalSn, publishTime, thesisLanguage, isCore, pdfId, evalResult, remark) {
+	var newRow = $("#templates tr").clone(true);
+	var currentRowId;
+	if ($("#contentTable").find(".rowId:last").length == 0) {
+		currentRowId = 0;
+	} else {
+		currentRowId = parseInt($("#contentTable").find(".rowId:last").text());
+	}
+	newRow.find(".rowId").text(currentRowId + 1);
+	newRow.find(".thesisId").text(thesisId);
+	newRow.find(".school").text(school);
+	newRow.find(".authorName").text(authorName);
+	newRow.find(".thesisTitle").text(thesisTitle);
+	newRow.find(".periodicalName").text(periodicalName);
+	newRow.find(".periodicalSn").text(periodicalSn);
+	newRow.find(".publishTime").text(publishTime);
+	newRow.find(".thesisLanguage").text(thesisLanguage);
+	newRow.find(".isCore").text(isCore);
+	newRow.find(".pdfId a").attr("href", "../downloadPDF?paper_id=" + thesisId);
+	newRow.find(".evalResult").val(evalResult);
+	newRow.find(".remark div").html(remark+"<span onclick=\"editRemark(this)\">编辑</span>");
+	
+	$("#contentTable tbody").append(newRow);
 }
 
 function cancelRemarkEditor() {
@@ -62,7 +87,7 @@ function saveData() {
 		var divText = $(itemUnsaved).find("div").text();
 		var obj = {
 			'tupleId': '23',
-			'evalResult': $(itemUnsaved).find(".evaluationResult").val(),
+			'evalResult': $(itemUnsaved).find(".evalResult").val(),
 			'remark': divText.substring(0, divText.length - 2),
 		};
 		if(objArrayStr==""){
@@ -96,4 +121,8 @@ function saveData() {
 		},
 		dataType: 'json'
 	});
+}
+
+function test(){
+	insertNewRow("123", "软院", "高光宇", "dfasdfasdf", "杂志", "123456", "12-14-12-123", "中文", "非核心", "pdf1234", "unselected", "很好很好很好很好很好很好很好很好很好很好");
 }
