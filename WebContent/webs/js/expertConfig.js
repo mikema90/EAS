912299517d77b6ae65c0fd9d83981dd3632f8e38
Gao Guangyu <330737668@qq.com>
2014-03-16 16:01:24 +0800

    modify delete expert func

--- WebContent/webs/js/expertConfig.js
+++ WebContent/webs/js/expertConfig.js
@@ -1,14 +1,14 @@
 // JavaScript Document
-inEditing=false;
+inEditing = false;
 
 $(document).ready(function () {
-	$("#exchangingDataBckgnd").css("visibility","visible");
-	$("#exchangingDataInnerWrapper").css("visibility","visible");
+	$("#exchangingDataBckgnd").css("visibility", "visible");
+	$("#exchangingDataInnerWrapper").css("visibility", "visible");
 	$.ajax({
 		type: 'POST',
 		url: "../getExpert",
 		success: function (jsonData) {
-			$("#exchangingDataInnerWrapper").css("visibility","hidden");
+			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
 			if (jsonData.Status == "success") {
 				$.each(jsonData.expert, function (idx, itemExpert) {
 					insertNewRow(itemExpert.id, itemExpert.college_id, itemExpert.name, itemExpert.work_id, itemExpert.subject, itemExpert.language);
@@ -16,12 +16,12 @@ $(document).ready(function () {
 			} else {
 				alert("加载失败");
 			}
-			$("#exchangingDataBckgnd").css("visibility","hidden");
+			$("#exchangingDataBckgnd").css("visibility", "hidden");
 		},
 		error: function () {
-			$("#exchangingDataInnerWrapper").css("visibility","hidden");
+			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
 			alert("加载失败");
-			$("#exchangingDataBckgnd").css("visibility","hidden");
+			$("#exchangingDataBckgnd").css("visibility", "hidden");
 		},
 		dataType: 'json'
 	});
@@ -33,14 +33,14 @@ function insertNewRow(tupleId, school, expertName, expertId, majors, languageAut
 	newRow.find(".school textarea").text(newRow.find(".school select option:selected").text());
 	newRow.find(".expertName textarea").text(expertName);
 	newRow.find(".expertId textarea").text(expertId);
-	newRow.find("input[name='tupleId']").attr("value",tupleId);
-	var majorArray=majors.split(",");
-	for(var i=0; i < majorArray.length; i++){
-		newRow.find("input[name='"+majorArray[i]+"']").attr("checked","checked");
+	newRow.find("input[name='tupleId']").attr("value", tupleId);
+	var majorArray = majors.split(",");
+	for (var i = 0; i < majorArray.length; i++) {
+		newRow.find("input[name='" + majorArray[i] + "']").attr("checked", "checked");
 	}
-	var languageAuthArray=languageAuths.split(",");
-	for(var i=0; i < languageAuthArray.length; i++){
-		newRow.find("input[name='"+languageAuthArray[i]+"']").attr("checked","checked");
+	var languageAuthArray = languageAuths.split(",");
+	for (var i = 0; i < languageAuthArray.length; i++) {
+		newRow.find("input[name='" + languageAuthArray[i] + "']").attr("checked", "checked");
 	}
 	$("#contentTable tbody").append(newRow);
 }
@@ -56,7 +56,7 @@ function insertNewRowForEdit(school, expertName, expertId, majors, languageAuths
 }
 
 function edit(targetElement) {
-	if(inEditing==true){
+	if (inEditing == true) {
 		alert("有修改未提交\n请先点击行末的“确定”提交\n或刷新页面。");
 		return;
 	}
@@ -71,19 +71,19 @@ function edit(targetElement) {
 	}
 	targetParent.removeClass("finishEdit");
 	targetParent.addClass("editing");
-	inEditing=true;
+	inEditing = true;
 }
 
 function confirmEdit(targetElement) {
-	$("#exchangingDataBckgnd").css("visibility","visible");
-	$("#exchangingDataInnerWrapper").css("visibility","visible");
+	$("#exchangingDataBckgnd").css("visibility", "visible");
+	$("#exchangingDataInnerWrapper").css("visibility", "visible");
 	var targetParent = $(targetElement).closest("tr");
-	var checkBoxes=targetParent.find("input[type=checkbox]");
-	for(var i=0;i<checkBoxes.length;i++){
+	var checkBoxes = targetParent.find("input[type=checkbox]");
+	for (var i = 0; i < checkBoxes.length; i++) {
 		//alert($(checkBoxes[i]).is(':checked'));
-		if($(checkBoxes[i]).is(':checked')==true){
+		if ($(checkBoxes[i]).is(':checked') == true) {
 			$(checkBoxes[i]).val("checked");
-		}else{
+		} else {
 			$(checkBoxes[i]).val("unchecked");
 		}
 	}
@@ -102,13 +102,13 @@ function finishEdit(targetParent) {
 	}
 	targetParent.addClass("finishEdit");
 	targetParent.removeClass("editing");
-	inEditing=false;
+	inEditing = false;
 }
 
 function submitForm(targetParent) {
-	var tempForm=$("<form></form>");
-	var clonedTarget=targetParent.clone(true);
-	tempForm=tempForm.append(clonedTarget);
+	var tempForm = $("<form></form>");
+	var clonedTarget = targetParent.clone(true);
+	tempForm = tempForm.append(clonedTarget);
 	var submitData = $(tempForm).serialize();
 	//alert(submitData);
 	$.ajax({
@@ -116,18 +116,18 @@ function submitForm(targetParent) {
 		url: "../addExpert",
 		data: submitData,
 		success: function (jsonData) {
-			$("#exchangingDataInnerWrapper").css("visibility","hidden");
+			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
 			if (jsonData.Status == "success") {
 				finishEdit(targetParent);
 			} else {
 				alert("添加/修改失败");
 			}
-			$("#exchangingDataBckgnd").css("visibility","hidden");
+			$("#exchangingDataBckgnd").css("visibility", "hidden");
 		},
 		error: function () {
-			$("#exchangingDataInnerWrapper").css("visibility","hidden");
+			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
 			alert("添加/修改失败");
-			$("#exchangingDataBckgnd").css("visibility","hidden");
+			$("#exchangingDataBckgnd").css("visibility", "hidden");
 		},
 		dataType: 'json'
 	});
@@ -135,86 +135,90 @@ function submitForm(targetParent) {
 
 function resetPassword(targetElement) {
 	var targetParent = $(targetElement).closest("tr");
-	if(inEditing==true){
+	if (inEditing == true) {
 		alert("有修改未提交\n请先点击行末的“确定”提交\n或刷新页面。");
 		return;
 	}
-	$("#exchangingDataBckgnd").css("visibility","visible");
-	$("#exchangingDataInnerWrapper").css("visibility","visible");
-	var expertId=targetParent.find(".expertId").text();
+	$("#exchangingDataBckgnd").css("visibility", "visible");
+	$("#exchangingDataInnerWrapper").css("visibility", "visible");
+	var expertId = targetParent.find(".expertId").text();
 	$.ajax({
 		type: 'POST',
 		url: "../resetExpertPWD",
-		data: {expertId:expertId},
+		data: {
+			expertId: expertId
+		},
 		success: function (jsonData) {
-			$("#exchangingDataInnerWrapper").css("visibility","hidden");
+			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
 			if (jsonData.Status == "success") {
 				alert("密码重置成功");
 			} else {
 				alert("重置密码失败");
 			}
-			$("#exchangingDataBckgnd").css("visibility","hidden");
+			$("#exchangingDataBckgnd").css("visibility", "hidden");
 		},
 		error: function () {
-			$("#exchangingDataInnerWrapper").css("visibility","hidden");
+			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
 			alert("重置密码失败");
-			$("#exchangingDataBckgnd").css("visibility","hidden");
+			$("#exchangingDataBckgnd").css("visibility", "hidden");
 		},
 		dataType: 'json'
 	});
 }
 
-function del(targetElement){
+function del(targetElement) {
 	var targetParent = $(targetElement).closest("tr");
-	if(targetParent.hasClass("finishEdit") && inEditing==true){
+	if (targetParent.hasClass("finishEdit") && inEditing == true) {
 		alert("有修改未提交\n请先点击行末的“确定”提交\n或刷新页面。");
 		return;
 	}
-	
-	$("#exchangingDataBckgnd").css("visibility","visible");
-	$("#exchangingDataInnerWrapper").css("visibility","visible");
-	
-	var expertId=targetParent.find(".expertId").text();
-	alert(expertId);
-		$.ajax({
+
+	$("#exchangingDataBckgnd").css("visibility", "visible");
+	$("#exchangingDataInnerWrapper").css("visibility", "visible");
+
+	var tupleId = targetParent.find("input[name='tupleId']").val();
+	//alert(expertId);
+	$.ajax({
 		type: 'POST',
 		url: "../deleteExpert",
-		data: {expertId:expertId},
+		data: {
+			tupleId: tupleId
+		},
 		success: function (jsonData) {
-			$("#exchangingDataInnerWrapper").css("visibility","hidden");
+			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
 			if (jsonData.Status == "success") {
 				targetParent.remove();
-				inEditing=false;
+				inEditing = false;
 			} else {
 				alert("删除失败");
 			}
-			$("#exchangingDataBckgnd").css("visibility","hidden");
+			$("#exchangingDataBckgnd").css("visibility", "hidden");
 		},
 		error: function () {
-			$("#exchangingDataInnerWrapper").css("visibility","hidden");
+			$("#exchangingDataInnerWrapper").css("visibility", "hidden");
 			alert("删除失败");
-			$("#exchangingDataBckgnd").css("visibility","hidden");
+			$("#exchangingDataBckgnd").css("visibility", "hidden");
 		},
 		dataType: 'json'
 	});
 }
 
 function testA() {
-	if(inEditing==true){
+	if (inEditing == true) {
 		alert("有修改未提交\n请先点击行末的“确定”提交\n或刷新页面。");
 		return;
 	}
 	insertNewRowForEdit("03000", "", "", "", "")
 }
 
-function onchangeSelect(targetElement){
+function onchangeSelect(targetElement) {
 	//alert($(targetElement).val());
-	var selectedValue=$(targetElement).val();
+	var selectedValue = $(targetElement).val();
 	$(targetElement).find("option[selected='selected']").removeAttr("selected");
-	$(targetElement).find("option[value='"+selectedValue+"']").attr("selected","selected");
+	$(targetElement).find("option[value='" + selectedValue + "']").attr("selected", "selected");
 }
 
-function onchangeTextArea(targetElement){
-	var textValue=$(targetElement).val();
+function onchangeTextArea(targetElement) {
+	var textValue = $(targetElement).val();
 	$(targetElement).text(textValue);
 }
